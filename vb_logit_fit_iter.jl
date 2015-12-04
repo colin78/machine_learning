ab733a020d303eaa9539d588b5bcdfbae5732a2a
colin78 <colinpawlowski78@gmail.com>
2015-12-04 16:36:17 -0500

    debugged lam function

--- vb_logit_fit_iter.jl
+++ vb_logit_fit_iter.jl
@@ -2,10 +2,10 @@ using DataFrames
 
 include("mfvb_functions.jl")
 
-X = readtable("data/example_coeff_X.csv", header=false)
-X_test = readtable("data/example_coeff_X_test.csv", header=false)
-y = readtable("data/example_coeff_y.csv", header=false)
-y_test = readtable("data/example_coeff_y_test.csv", header=false)
+X = convert(Array,readtable("data/example_coeff_X.csv", header=false))
+X_test = convert(Array,readtable("data/example_coeff_X_test.csv", header=false))
+y = convert(Array,readtable("data/example_coeff_y.csv", header=false))
+y_test = convert(Array,readtable("data/example_coeff_y_test.csv", header=false))
 
 # function vb_logit_fit_iter(X,y)
 N, D = size(X)
@@ -16,18 +16,15 @@ invV = eye(D) * D
 logdetV = - D * log(D)
 w = zeros(D, 1)
 
-L_last = 0.5 * (logdetV_xi + w' * invV_xi * w) - log(2)
-
-
-for n = 1:N;
-    xn = X(n,:)'
+for n = 1:N
+    xn = X[n,:]'
 
     # precompute values
     Vx = V * xn
     VxVx = Vx * Vx'
-    c = xn' * Vx
+    c = (xn' * Vx)[1]
     xx = xn * xn'
-    t_w = invV * w + 0.5 * y(n) * xn
+    t_w = invV * w + 0.5 * y[n] * xn
 
     # start iteration at xi = 0, lam_xi = 1/8
     V_xi = V - VxVx / (4 + c)
@@ -35,11 +32,11 @@ for n = 1:N;
     logdetV_xi = logdetV - log(1 + c / 4)
     w = V_xi * t_w
     
-    L_last = 0.5 * (logdetV_xi + w' * invV_xi * w) - log(2)
+    L_last = 0.5 * (logdetV_xi + w' * invV_xi * w)[1] - log(2)
 
     for i = 1:max_iter
         # update xi by EM algorithm
-        xi = sqrt(xn' * (V_xi + w * w') * xn)
+        xi = sqrt((xn' * (V_xi + w * w') * xn)[1])
         lam_xi = lam(xi)
 
         # Sherman-Morrison formula and Matrix determinant lemma
@@ -52,17 +49,17 @@ for n = 1:N;
 
         # variational bound must grow!
         if L_last > L
-            fprintf('Last bound %6.6f, current bound %6.6f\n', L_last, L)
-            error('Variational bound should not reduce')
+            println("Last bound %6.6f, current bound %6.6f\n", L_last, L)
+            println("Variational bound should not reduce")
         end
         # stop if change in variation bound is < 0.001%
         if abs(L_last - L) < abs(0.00001 * L)
             break
         end
-        L_last = L;
+        L_last = L
     end
     if i == max_iter
-        println('Bayesian logistic regression reached maximum number of iterations.'
+        println("Bayesian logistic regression reached maximum number of iterations.")
     end
     
     V = V_xi
