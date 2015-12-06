cdbac9f1907b9422b927403151f69d9654ccd8d5
colin78 <colinpawlowski78@gmail.com>
2015-12-05 21:23:12 -0500

    added lines to plots

--- test.jl
+++ test.jl
@@ -12,10 +12,14 @@ y_test = convert(Array,readtable("data/example_coeff_y_test.csv", header=false))
 X_for_plot = DataFrame(x1=X[:,2],x2=X[:,3],y=y[:])
 X_for_plot2 = DataFrame(x1=X_test[:,2],x2=X_test[:,3],y=y_test[:])
 
+equation(x1) = x1
+equation
+
 plot1 = plot(layer(X_for_plot, x="x1", y="x2", Geom.point, color = "y"),
-			Scale.color_discrete_manual("red","blue"));
+			Scale.color_discrete_manual("red","blue"),
+			layer(equation, -3, 3, Theme(default_color=color("black"))));
 draw(PDF("figures/train.pdf", 6inch, 4inch), plot1)
 
-plot2 = plot(layer(X_for_plot2, x="x1", y="x2", Geom.point, color = "y"),
-			Scale.color_discrete_manual("red","blue"));
-draw(PDF("figures/test.pdf", 6inch, 4inch), plot2)
+# plot2 = plot(layer(X_for_plot2, x="x1", y="x2", Geom.point, color = "y"),
+# 			Scale.color_discrete_manual("red","blue"));
+# draw(PDF("figures/test.pdf", 6inch, 4inch), plot2)
