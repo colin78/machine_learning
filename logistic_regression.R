1e53b39ef307c6bfd5df25bec1edbe6f86acf121
colin78 <colinpawlowski78@gmail.com>
2015-12-06 00:10:31 -0500

    test.jl updates

---
+++ logistic_regression.R
@@ -0,0 +1,5 @@
+X = read.csv("data/example_coeff_X.csv", header=FALSE)
+y = read.csv("data/example_coeff_y.csv", header=FALSE)
+X_test = read.csv("data/example_coeff_X_test.csv", header=FALSE)
+y_test = read.csv("data/example_coeff_y_test.csv", header=FALSE)
+
--- test.jl
+++ test.jl
@@ -14,19 +14,19 @@ y_test = convert(Array,readtable("data/example_coeff_y_test.csv", header=false))
 X_for_plot = DataFrame(x1=X[:,2],x2=X[:,3],y=y[:])
 X_for_plot2 = DataFrame(x1=X_test[:,2],x2=X_test[:,3],y=y_test[:])
 
-# Matrix_iter = vb_logit_fit_iter(X,y)
-# Matrix = vb_logit_fit(X,y)
+Matrix_iter = vb_logit_fit_iter(X,y)
+Matrix = vb_logit_fit(X,y)
 w_iter = Matrix_iter[:,1]
 w = Matrix[:,1]
 
-equation(x1) = (-w_iter[1] - w_iter[2]*x1)/w_iter[3]
-equation2(x1) = (-w[1] - w[2]*x1)/w[3]
+# equation(x1) = (-w_iter[1] - w_iter[2]*x1)/w_iter[3]
+# equation2(x1) = (-w[1] - w[2]*x1)/w[3]
 
-plot1 = plot(layer(X_for_plot, x="x1", y="x2", Geom.point, color = "y"),
-			Scale.color_discrete_manual("red","blue"),
-			layer(equation, -3, 3, Theme(default_color=color("green"))),
-			layer(equation2, -3, 3, Theme(default_color=color("orange"))));
-draw(PDF("figures/train.pdf", 6inch, 4inch), plot1)
+# plot1 = plot(layer(X_for_plot, x="x1", y="x2", Geom.point, color = "y"),
+# 			Scale.color_discrete_manual("red","blue"),
+# 			layer(equation, -3, 3, Theme(default_color=color("green"))),
+# 			layer(equation2, -3, 3, Theme(default_color=color("orange"))));
+# draw(PDF("figures/train.pdf", 6inch, 4inch), plot1)
 
 # plot2 = plot(layer(X_for_plot2, x="x1", y="x2", Geom.point, color = "y"),
 # 			Scale.color_discrete_manual("red","blue"),
