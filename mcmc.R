eca0b1662e4ea9b7690a754aeb5831f2bf714015
colin78 <colinpawlowski78@gmail.com>
2015-12-09 18:56:24 -0500

    created contour plots

--- mcmc.R
+++ mcmc.R
@@ -29,9 +29,6 @@ V_iter = read.csv("results/V_iter.csv", header=FALSE)
 
 textsize = 18
 
-
-samples[,1]
-
 # Plot MCMC sample results
 library(ggplot2)
 # # see https://github.com/wch/fontcm
@@ -44,70 +41,56 @@ loadfonts()
 library(MASS)
 indep = posterior[seq(1,10000,10),]
 indep2 = posterior2[seq(1,10000,10),]
-
+summary(posterior)
 
 g = ggplot(data=indep, aes(x=x1, y=x2)) + geom_point() +
   theme(text=element_text(size=textsize, family="CM Sans")) + 
   xlab(expression(w[1])) + ylab(expression(w[2]))
 g = g + geom_density2d()
+g
 ggsave("figures/mcmc_uniform_2d.pdf", g, width=8, height=5)
 
-mu = w_iter[2:3,]
-sigma <- as.matrix(V[2:3,2:3]) # matrix(c(1, 0.6, 0.6, 1), nrow=2)
-rng   <- 15
-N     <- 50
-X     <- seq(mu[1]-rng*sigma[1, 1], mu[1]+rng*sigma[1, 1], length.out=N)
-Y     <- seq(mu[2]-rng*sigma[2, 2], mu[2]+rng*sigma[2, 2], length.out=N)
-set.seed(123)
-library(mvtnorm)
-genZ <- function(x, y) { dmvnorm(cbind(x, y), mu, sigma) }
-matZ <- outer(X, Y, FUN="genZ")
-c = contour(X, Y, matZ, nlevels=5, col="blue", drawlabels=FALSE,
-            main="Contours for 2D-normal density",
-            xlab=expression(x[1]), ylab=expression(x[2]), lwd=2)
-pdf("figures/mcmc_uniform_2d_2.pdf", width=8, height=5)
-c
-dev.off()
+mu = w[2:3,]
+sigma = as.matrix(V[2:3,2:3])
+mu_iter = w[2:3,]
+sigma_iter <- as.matrix(V_iter[2:3,2:3])
 
 library(mvtnorm)
 library(ggplot2)
 set.seed(1)
-n <- 1e3
+c99 = qchisq(.99, df=2)
 c95 <- qchisq(.95, df=2)
 c90 = qchisq(.9, df=2)
-c65 = qchisq(.65, df=2)
-rho <- 0.8  #correlation 
-Sigma <- matrix(c(1, rho, rho, 1), 2, 2) # Covariance matrix
-x <- rmvnorm(n, mean=c(0, 0), Sigma)
-z  <- p95 <- rep(NA, n)
-for(i in 1:n){
-  z[i] <- x[i, ] %*% solve(Sigma, x[i, ])
-  p95[i] <- (z[i] < c95)
-}
-data <- data.frame(x, z, p95)
-p <- ggplot(data, aes(X1, X2)) + geom_point(aes(colour = p95))
-p + stat_ellipse(type = "norm")
-p <- ggplot(data, aes(X1, X2)) + geom_point(aes(colour = p95))
-p + stat_ellipse(type = "norm")
+c50 = qchisq(.5, df=2)
 
-ellips <- function(center = c(0,0), c=c95, rho=-0.8, npoints = 100){
+ellips <- function(center = c(0,0), c=c95, sigma=sigma, npoints = 100){
   t <- seq(0, 2*pi, len=npoints)
-  Sigma <- matrix(c(1, rho, rho, 1), 2, 2)
-  a <- sqrt(c*eigen(Sigma)$values[2])
-  b <- sqrt(c*eigen(Sigma)$values[1])
-  x <- center[1] + a*cos(t)
-  y <- center[2] + b*sin(t)
-  X <- cbind(x, y)
-  R <- eigen(Sigma)$vectors
-  data.frame(X%*%R)
+  a <- sqrt(c*eigen(sigma)$values[2])
+  b <- sqrt(c*eigen(sigma)$values[1])
+  x <- a*cos(t)
+  y <- b*sin(t)
+  X <- cbind(x,y)
+  R <- eigen(sigma)$vectors
+  df = X%*%R + matrix(rep(center,npoints),npoints,2, byrow=T)
+  data.frame(df)
 }
-dat <- ellips(center=c(0, 0), c=c95, rho, npoints=100)
-dat90 <- ellips(center=c(0, 0), c=c90, rho, npoints=100)
-dat65 <- ellips(center=c(0, 0), c=c65, rho, npoints=100)
 
-p + geom_path(data=dat, aes(x=X1, y=X2), colour='blue') + 
- geom_path(data=dat90, aes(x=X1, y=X2), colour='blue') + 
- geom_path(data=dat65, aes(x=X1, y=X2), colour='blue')
+dat <- ellips(center=mu, c=c95, sigma, npoints=100)
+dat90 <- ellips(center=mu, c=c90, sigma, npoints=100)
+dat99 <- ellips(center=mu, c=c99, sigma, npoints=100)
+dat50 <- ellips(center=mu, c=c50, sigma, npoints=100)
+
+g = ggplot(data=indep, aes(x=x1, y=x2)) +
+  theme(text=element_text(size=textsize, family="CM Sans")) + 
+  xlab(expression(w[1])) + ylab(expression(w[2]))
+g = g + geom_density2d(aes(x=x1, y=x2, colour = "MCMC"))
+g = g + geom_path(data=dat, aes(x=X1, y=X2, colour='MFVB')) + 
+ geom_path(data=dat90, aes(x=X1, y=X2, colour='MFVB')) +
+ geom_path(data=dat99, aes(x=X1, y=X2, colour='MFVB')) +
+ geom_path(data=dat50, aes(x=X1, y=X2, colour='MFVB')) +
+  scale_colour_manual("", values = c("MFVB" = "red", "MCMC"="blue"))
+g
+ggsave("figures/mcmc_uniform_mfvb.pdf", g, width=8, height=5)
 
 
 require(mvtnorm)
@@ -149,3 +132,18 @@ g2 = ggplot(data=samples, aes(x=w1, y=w2)) + geom_point() +
   theme(text=element_text(size=textsize, family="CM Sans")) + 
   xlab(expression(w[1])) + ylab(expression(w[2]))
 g2 + geom_density2d()
+
+rng   <- 15
+N     <- 50
+X     <- seq(mu[1]-rng*sigma[1, 1], mu[1]+rng*sigma[1, 1], length.out=N)
+Y     <- seq(mu[2]-rng*sigma[2, 2], mu[2]+rng*sigma[2, 2], length.out=N)
+set.seed(123)
+library(mvtnorm)
+genZ <- function(x, y) { dmvnorm(cbind(x, y), mu, sigma) }
+matZ <- outer(X, Y, FUN="genZ")
+c = contour(X, Y, matZ, nlevels=5, col="blue", drawlabels=FALSE,
+            main="Contours for 2D-normal density",
+            xlab=expression(x[1]), ylab=expression(x[2]), lwd=2)
+pdf("figures/mcmc_uniform_2d_2.pdf", width=8, height=5)
+c
+dev.off()
